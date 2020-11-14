5db6f93ba278ebe0b8490025eda04796fba52c95
Luke V <wulfstawulfsta@gmail.com>
2020-11-13 22:28:25 -0500

    Revert "tensorflow-rocm: init source build at 2.3.0"
    This reverts commit 24db0cfea8d6f24bb832d56c6d3d8a6f28ee1004.

--- pkgs/all-packages.nix
+++ pkgs/all-packages.nix
@@ -208,7 +208,7 @@ with pkgs;
     opencl = self.rocm-opencl-runtime;
   };
 
-  tensorflow-rocm-bin = python37Packages.callPackage ./development/libraries/tensorflow/1/bin/bin.nix {
+  tensorflow-rocm = python37Packages.callPackage ./development/libraries/tensorflow/bin.nix {
     inherit (self) miopen-hip miopengemm rocrand
                    rocfft rocblas rocm-runtime rccl cxlactivitylogger;
     hip = self.hip-clang;
@@ -231,17 +231,11 @@ with pkgs;
   #   };
   # };
 
-  # tensorflow2-rocm-bin = self.tf2PyPackages.pkgs.callPackage ./development/libraries/tensorflow/2/bin.nix {
+  # tensorflow2-rocm = self.tf2PyPackages.pkgs.callPackage ./development/libraries/tensorflow/bin2.nix {
   #   inherit (self) hcc hcc-unwrapped miopen-hip miopengemm rocrand
   #                  rocfft rocblas rocm-runtime rccl cxlactivitylogger;
   #   hip = self.hip;
   # };
-
-  tensorflow2-rocm = python3Packages.callPackage ./development/libraries/tensorflow/2 {
-    inherit (self) rocm-runtime rccl rocprim hipcub rocsparse hipsparse rocblas 
-                   miopengemm miopen-hip rocrand rocfft;
-    hip = self.hip-clang;
-  };
 
   # pytorch-rocm = python37Packages.callPackage ./development/libraries/pytorch/default.nix {
   #   inherit (self) rocm-runtime miopengemm rocsparse hipsparse rocthrust
